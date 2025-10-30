d8b6e8e0f41ff00ef421283142f1f528cd871609
dheeraj polawar <dheerajpolawar233@gmail.com>
2025-10-30 09:34:46 +0530

    CheckVersion

---
+++ src/language/basics/CheckVersion.java
@@ -0,0 +1,7 @@
+package language.basics;
+
+public class CheckVersion {
+    public static void main(String[] args) {
+        System.out.println("Running Java version: " + System.getProperty("java.version"));
+    }
+}
