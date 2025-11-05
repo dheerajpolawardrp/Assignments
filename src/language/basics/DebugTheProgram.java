47f1761a9e19ee51358fc76f4c156b5ecac7e392
dheeraj polawar <dheerajpolawar233@gmail.com>
2025-11-05 13:29:21 +0530

    DebugTheProgram

---
+++ src/language/basics/DebugTheProgram.java
@@ -0,0 +1,22 @@
+package language.basics;
+
+public class DebugTheProgram {
+
+	public static void main(String[] args) {
+int i = 1;
+
+System.out.println("Step 1: Program starts");
+System.out.println("Step 2: Preparinig to execute next step");
+System.out.println("Step 3: Performin calculations");
+
+int j = i+1;
+System.out.println(1/j);
+System.out.println("Program execution completed");
+	
+//when error comes in java program 	
+//The first line of the error is always going to give you the information about the error
+// and the documentation location 
+// and last line of the error is going to give you the location of an error that occured in program
+	}
+
+}
