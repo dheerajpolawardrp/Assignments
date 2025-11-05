1304dfa9c5b4b58f63a2edc044ac79a00679ae5d
dheeraj polawar <dheerajpolawar233@gmail.com>
2025-11-05 18:07:01 +0530

    Assignment1

---
+++ src/assignments/Assignment1.java
@@ -0,0 +1,73 @@
+package assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		float temperature = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius is " + temperature);
+		
+		boolean order = true;
+		System.out.println("customer has placed an order is " + order);
+		
+		short phonenumber = 123-456-7890;
+		System.out.println("Person's phone number is "   + phonenumber);
+		
+		float amount = 1000.50f;
+		System.out.println("Amount of money in a customer's bank account is " + amount);
+		
+		String email = "john.doe@example.com";
+		System.out.println("Person's email address is " + email);
+		
+		float latitude = 37.7749f;
+		System.out.println("Coordinates of a location (latitude) is " + latitude);
+				
+		float longitude = -122.4194f;
+		System.out.println("Coordinates of a location (longitude) is " + longitude);
+		
+		boolean status = true;
+		System.out.println("Person's marital status is " + status);
+		
+		String occupation = "Software Engineer";
+		System.out.println("Person's occupation " + occupation);
+		
+		String color = "Blue";
+		System.out.println("Person's favourite colour is " + color);
+		
+		short currentyear = 2025;
+		System.out.println("Current year is " + currentyear);
+		
+		int followers = 1000000;
+		System.out.println("Number of followers on a social media platform are " + followers);
+		
+		float movierating = 7.5f;
+		System.out.println("Rating of a movie is " + movierating);
+		
+		char bloodtype = 'A';
+		System.out.println("Person's blood type is " + bloodtype);
+	
+		String booktitle = "To Kill a Mockingbird";
+		System.out.println("Title of a book is " + booktitle);
+		
+		short employees = 500;
+		System.out.println("Number of employees in a company are " + employees);
+		
+		String timespan = "2:30 PM";
+		System.out.println("Time of an event is " + timespan);
+		
+		String companyname = "United States";
+		System.out.println("Name of a country is " + companyname);
+		
+		String eyecolor = "Brown";
+		System.out.println("Person's eye color is " + eyecolor);
+		
+		String birthplace = "New York City";
+		System.out.println("Person's birthplace is " + birthplace);
+		
+		float distance = 200.5f;
+		System.out.println("Distance between two cities is " + distance);
+	
+	
+	}
+
+}
