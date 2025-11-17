1730bd21fdca57b491ea65e5d50a7adf9306e307
dheeraj polawar <dheerajpolawar233@gmail.com>
2025-11-17 23:34:10 +0530

    Assignment10NewClass

---
+++ src/assignments/Assignment10NewClass.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class Assignment10NewClass {
+
+	public static void main(String[] args) {
+
+		Assignments10CommonMethods a10 = new Assignments10CommonMethods();
+
+		a10.emplooyeeHike("Alice Johnson", 75000.0, 15, 1500, 5000);
+
+		a10.emplooyeeHike("Bob Smith", 82000.0, 10, 1200, 0);
+
+		a10.emplooyeeHike("Carol Davis", 82000.0, 15, 1500, 5000);
+
+		a10.emplooyeeHike("David Brown", 90000.0, 15, 300, 5000);
+
+		a10.emplooyeeHike("Eva Green", 60000.0, 15, 1200, 0);
+	}
+
+}
